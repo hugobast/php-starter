23f6b5b47c5221a1101c4106b1f231dca7542b7e
Hugo Bastien <hugobast@gmail.com>
2012-09-26 10:05:21 -0400

    Folder and file structure

---
+++ scripts/config.rb
@@ -0,0 +1,8 @@
+http_path = "/"
+css_dir = "../assets/stylesheets/"
+sass_dir = "../assets/stylesheets/scss"
+images_dir = "../assets/images"
+javascripts_dir = "javascripts"
+
+# relative to the css file
+http_generated_images_path = "../images"
